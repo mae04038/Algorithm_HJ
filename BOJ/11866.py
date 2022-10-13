258a375cde16c13eb54e308e06f317b75f7fbe6f
heejeong Jang <mae04038@naver.com>
2022-10-13 17:59:26 +0900

    Solved: 큐

--- BOJ/11866.py
+++ BOJ/11866.py
@@ -1,13 +1,30 @@
+from collections import deque
 N, K = map(int, input().split())
-data = []
+q = deque()
 result = []
 for i in range(1, N+1):
-    data.append(i)
-while len(data) > 0:
-    if len(data) < K:
-        
-    else:
-        num = data.pop(2)
-        result.append(num)
+    q.append(i)
+# print(q)
+print("<", end="")
+while q:
+    for i in range(K-1):
+        q.append(q[0])
+        q.popleft()
+    print(q.popleft(), end="")
+    if q:
+        print(", ", end="")
+print(">")
 
-print(result)
+'''
+while q:
+    q.rotate(-(K-1))
+    result.append(q.popleft())
+
+for i in range(N):
+    if i == 0:
+        print("<%d, "%result[i], end="")
+    elif i == N-1:
+        print("%d>"%result[i], end="")
+    else:
+        print("%d, "%result[i], end="")
+'''
