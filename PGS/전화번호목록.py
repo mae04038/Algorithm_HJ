8386d1329d0de1acae3d3c56530debaf26c2250e
heejeong Jang <mae04038@naver.com>
2022-11-04 00:15:47 +0900

    Solved

---
+++ PGS/전화번호목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    answer = True
+    phoneBook = sorted(phone_book) # 문자열abc 순으로 정렬됨
+    # 접두사여야 함.
+    for i, j in zip(phoneBook, phoneBook[1:]):
+        if j.startswith(i): 
+            answer = False
+         
+    
+    return answer
