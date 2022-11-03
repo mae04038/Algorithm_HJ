a1bf9f44fe56e7f02fa0551b3815ec407f58fd55
heejeong Jang <mae04038@naver.com>
2022-11-04 02:53:16 +0900

    Solved: bfs

---
+++ BOJ/13549.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+visited = [False] * 100001 # 방문처리 -> 메모리초과 해결
+q = deque()
+q.append((N, 0)) # 위치, 걸린 시간
+visited[N] = 1
+while q:
+    res, time = q.popleft()
+    if res == K:
+        answer = time
+        break
+    if (0<= res*2 <= 100000) and not visited[res*2]:
+        q.append((res*2, time))
+        visited[res*2] = 1
+    if (0 <= res - 1 <= 100000) and not visited[res-1]:
+        q.append((res-1, time+1))
+        visited[res-1] = 1
+    if (0 <= res + 1 <= 100000) and not visited[res+1]:
+        q.append((res+1, time+1))
+        visited[res+1] = 1
+
+print(answer)
