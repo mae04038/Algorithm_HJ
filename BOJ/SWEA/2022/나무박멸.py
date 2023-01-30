55cca86f79684e742b07ebb5a7516b0cc89cb913
heejeong Jang <mae04038@naver.com>
2023-01-31 01:38:42 +0900

    Progress: 다른 테케확인하기

---
+++ BOJ/SWEA/2022/나무박멸.py
@@ -0,0 +1,108 @@
+import copy
+n, m, k, c = map(int, input().split()) # 격자 크기, 박멸진행년수, 제초제 범위, 제초제 남아있는 년수
+trees = [list(map(int, input().split())) for _ in range(n)]
+answer = 0 # m년 동안 박멸한 나무의 그루 
+
+gx = [-1, 1, 0, 0]
+gy = [0, 0, 1, -1]
+dx_dy = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+def grow():
+    for i in range(n):
+        for j in range(n):
+            if trees[i][j] < -1:
+                trees[i][j] += 1
+            # if trees[i][j] == -2:
+            #     trees[i][j] = 0
+            
+            if trees[i][j] >= 1:
+                cnt = 0
+                for k in range(4):
+                    x, y = gx[k] + i, gy[k] + j
+                    if x < 0 or x >= n or y < 0 or y >= n:
+                        continue
+                    if trees[x][y] >= 1:
+                        cnt += 1
+                trees[i][j] += cnt
+
+def spread():
+    tmp_tree = copy.deepcopy(trees)
+    for i in range(n):
+        for j in range(n):
+            if tmp_tree[i][j] >= 1:
+                cnt = 0
+                for k in range(4):
+                    x, y = gx[k] + i, gy[k] + j
+                    if x < 0 or x >= n or y < 0 or y >= n:
+                        continue
+                    if tmp_tree[x][y] == 0:
+                        cnt += 1
+                if cnt != 0:
+                    spread_trees = trees[i][j] // cnt
+                else:
+                    spread_trees = 0
+                # print(spread_trees) # 확인용
+                for k in range(4):
+                    x, y = gx[k] + i, gy[k] + j
+                    if x < 0 or x >= n or y < 0 or y >= n:
+                        continue
+                    if tmp_tree[x][y] == 0:
+                        trees[x][y] += spread_trees
+
+def cnt_remove(x, y):
+    cnt = 0
+    for nx, ny in dx_dy:
+        for i in range(1, k+1):
+            cx, cy = x + nx*i, y + ny*i
+            if cx < 0 or cx >= n or cy < 0 or cy >= n or trees[cx][cy] == -1:
+                break
+            cnt += trees[cx][cy]
+    return cnt
+
+def put_remove(x, y):
+    trees[x][y] = -(c+2)
+    for nx, ny in dx_dy:
+        for i in range(1, k+1):
+            cx, cy = x + nx*i, y + ny*i
+            if cx < 0 or cx >= n or cy < 0 or cy >= n or trees[cx][cy] == -1:
+                break
+            trees[cx][cy] = -(c+2)
+
+def remove():
+    tmp_tree = copy.deepcopy(trees)
+    max_num = 0
+    final_x, final_y = 0, 0
+    for i in range(n):
+        for j in range(n):
+            if trees[i][j] == -2: # 제초제 뿌릴 때 이전제초제 제거가능하면 제거
+                trees[i][j] = 0
+
+            if trees[i][j] >= 1:
+                tmp_tree[i][j] += cnt_remove(i, j) # 제초하게 될 나무 그루 수 계산함수
+                # print(tmp_tree[i][j]) # 제초할 나무 개수 확인용
+                if tmp_tree[i][j] > max_num:
+                    max_num = tmp_tree[i][j]
+                    final_x, final_y = i, j # 최종 제초제위치
+    put_remove(final_x, final_y)
+    
+    return max_num
+    
+
+for _ in range(m):
+    # 1. 성장
+    grow()
+    # print(trees) # 확인용
+
+    # 2. 번식
+    spread()
+    # print(trees) # 확인용
+
+    # 3. 제초제 뿌리기
+    # 제초제 뿌릴 위치 선택
+    # 제초제 뿌리기 - c년 동안 유지 => 해당위치 -(c+2) 로 해주고 -2되면 0으로 바꿔주기
+    answer += remove()
+    # print(trees)
+    
+    print(answer)
+
+    
