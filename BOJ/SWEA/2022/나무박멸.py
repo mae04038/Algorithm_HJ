5f15e2c27a190bc6bd2a90d48054179818185283
heejeong Jang <mae04038@naver.com>
2023-02-01 00:28:39 +0900

    Solved

--- BOJ/SWEA/2022/나무박멸.py
+++ BOJ/SWEA/2022/나무박멸.py
@@ -2,6 +2,7 @@ import copy
 n, m, k, c = map(int, input().split()) # 격자 크기, 박멸진행년수, 제초제 범위, 제초제 남아있는 년수
 trees = [list(map(int, input().split())) for _ in range(n)]
 answer = 0 # m년 동안 박멸한 나무의 그루 
+gas_graph = [[-1] * n for _ in range(n)]
 
 gx = [-1, 1, 0, 0]
 gy = [0, 0, 1, -1]
@@ -59,13 +60,41 @@ def cnt_remove(x, y):
             cnt += trees[cx][cy]
     return cnt
 
-def put_remove(x, y):
+def put_remove(x, y): # 제초제 놓기
+    # 도중에 벽(-1)이 있거나 나무가 아예 없는 칸(0)이 있으면 해당 칸까지 뿌리고 멈춤
+    # global gas_graph
+    # gas_graph[x][y] = c
+    # if trees[x][y] != 0:
+    #     for nx, ny in dx_dy:
+    #         for i in range(1, k+1):
+    #             cx, cy = x + nx*i, y + ny*i
+    #             if cx < 0 or cx >= n or cy < 0 or cy >= n :
+    #                 break
+    #             if trees[cx][cy] > 0:
+    #                 gas_graph[cx][cy] = c
+    #                 trees[cx][cy] = 0
+    #             elif trees[cx][cy] == 0:
+    #                 gas_graph[cx][cy] = c
+    #                 break
+    #             elif trees[cx][cy] == -1:
+    #                 break
+
+    ##########################
     trees[x][y] = -(c+2)
     for nx, ny in dx_dy:
         for i in range(1, k+1):
             cx, cy = x + nx*i, y + ny*i
-            if cx < 0 or cx >= n or cy < 0 or cy >= n or trees[cx][cy] == -1:
+            if cx < 0 or cx >= n or cy < 0 or cy >= n :
                 break
+            # if trees[cx][cy] == 0 or trees[cx][cy] == -1: # 나무가 없는 칸
+            if trees[cx][cy] <= 0:
+                if trees[cx][cy] != -1 and trees[cx][cy] == 0:
+                    trees[cx][cy] = -(c+2)
+                if trees[cx][cy] <= -2:
+                    trees[cx][cy] -= c
+                
+                break
+            
             trees[cx][cy] = -(c+2)
 
 def remove():
@@ -86,23 +115,30 @@ def remove():
     put_remove(final_x, final_y)
     
     return max_num
-    
+
+def down_remove():
+    for i in range(n):
+        for j in range(n):
+            if gas_graph[i][j] > -1:
+                gas_graph[i][j] -= 1
+
 
 for _ in range(m):
     # 1. 성장
     grow()
-    # print(trees) # 확인용
+    print(trees) # 확인용
 
     # 2. 번식
     spread()
-    # print(trees) # 확인용
+    print(trees) # 확인용
 
     # 3. 제초제 뿌리기
     # 제초제 뿌릴 위치 선택
     # 제초제 뿌리기 - c년 동안 유지 => 해당위치 -(c+2) 로 해주고 -2되면 0으로 바꿔주기
     answer += remove()
-    # print(trees)
+    print(trees)
+    down_remove() #제초제 유지년수 계산
     
-    print(answer)
+print(answer)
 
     
